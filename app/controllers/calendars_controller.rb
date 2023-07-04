3bba823ac571d5e52f6f4fc5cee9c7c4632e97e6
kinomomo <snowveil6@gmail.com>
2023-07-04 13:49:40 +0900

    Issue1修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,9 +1,9 @@
-class calendarsController < ApplicationController
+class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
-    @plan = plan_new
+    @plan = Plan.new
   end
 
   # 予定の保存
@@ -18,25 +18,24 @@ class calendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_week
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @todays_date = date_today
+    @todays_date = Date.today.day
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
-    plans = plan_where(date: @todays_date..@todays_date + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: '(@todays_date + x).month', date: '(@todays_date+x).day', plans: 'today_plans'}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
-
   end
 end
